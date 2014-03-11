ee8fb8d523db7c9caae5d1528884b8baa0d30117
pablobarbera <pablobarbera@gmail.com>
2014-03-11 15:46:07 -0400

    authentication now works in RStudio

--- Rfacebook/R/fbOAuth.R
+++ Rfacebook/R/fbOAuth.R
@@ -33,11 +33,6 @@
 #' everything works well, you will get a message that says you can return to R. If not,
 #' try again in a few minutes to make sure your app had its settings updated properly.
 #'
-#' Note that the authentication process will NOT work correctly in RStudio. As an
-#' alternative, create your token in R from the command line interface or the R
-#' application, save it as a file, and then open it up from RStudio.
-#' 
-#'
 #' @author
 #' Pablo Barbera \email{pablo.barbera@@nyu.edu}
 #' @seealso \code{\link{getUsers}}, \code{\link{getPost}}, \code{\link{searchFacebook}}
