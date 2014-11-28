e1d9c7a3f2449b3a2b768b6167deecb2d16cc8ad
amitvirat <amit.virat@gmail.com>
2014-11-29 01:40:59 +0530

    mutual friend changes

--- Rfacebook/R/getNetwork.R
+++ Rfacebook/R/getNetwork.R
@@ -51,7 +51,7 @@ getNetwork <- function(token, format='edgelist', verbose=TRUE){
 	n <- length(friends$id)
 	if (verbose==TRUE){ pb <- txtProgressBar(min=1,max=n, style=3) }
 	for (i in 1:n){
-		query <- paste0("https://graph.facebook.com/user.context/mutualfriends/", friends$id[i], "?")
+		query <- paste0("https://graph.facebook.com/user.context/mutual_friends/", friends$id[i], "?")
 		content <- callAPI(query, token)
 		mutual.friends <- unlist(lapply(content[[1]], '[[', 'name'))
 		for (friend in mutual.friends){
