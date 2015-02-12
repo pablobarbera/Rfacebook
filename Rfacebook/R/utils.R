c35af530bd3ddb6ab01facecd5897e4bd3c77cdb
Pablo Barberá <pablo.barbera@nyu.edu>
2015-02-12 07:43:18 -0500

    getInsights now works with country metric

--- Rfacebook/R/utils.R
+++ Rfacebook/R/utils.R
@@ -48,14 +48,32 @@ pageDataToDF <- function(json){
 	return(df)
 }
 
-insightsDataToDF <- function(json, values){
-  df <- data.frame(
-    id = unlistWithNA(json, 'id'),
-    metric_name = unlistWithNA(json, 'name'),
-    period = unlistWithNA(json, 'period'),
-    values = unlistWithNA(values, 'value'),
-    end_time = unlistWithNA(values, 'end_time'),
-    stringsAsFactors=F)
+insightsDataToDF <- function(json, values, metric){
+  if (metric!="page_fans_country"){
+    df <- data.frame(
+        id = unlistWithNA(json, 'id'),
+        metric_name = unlistWithNA(json, 'name'),
+        period = unlistWithNA(json, 'period'),
+        values = unlistWithNA(values, 'value'),
+        end_time = unlistWithNA(values, 'end_time'),
+        stringsAsFactors=F)
+  }
+  if (metric=="page_fans_country"){
+    # values for country-level variables
+    countries <- lapply(json[[1]]$values, function(x) names(x$value))
+    values <- lapply(json[[1]]$values, function(x) x$value)
+    end_times <- unlist(lapply(json[[1]]$values, function(x) x$end_time))
+    end_times <- unlist(lapply(1:length(countries), function(x) rep(end_times[[x]], length(countries[[x]]))))
+
+    df <- data.frame(
+        id = unlistWithNA(json, 'id'),
+        metric_name = unlistWithNA(json, 'name'),
+        period = unlistWithNA(json, 'period'),
+        country = unlist(countries),
+        values = unlist(values),
+        end_time = unlist(end_times),
+        stringsAsFactors=F)
+    }
   return(df)
 }
 
