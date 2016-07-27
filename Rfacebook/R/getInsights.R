8a086b401c9c8e7318a858871e10a3d17ef6cd2c
Pablo Barberá <pablo.barbera@nyu.edu>
2016-07-26 23:36:12 -0700

    added info message for data > 2 years old in getInsights (fix #76)

--- Rfacebook/R/getInsights.R
+++ Rfacebook/R/getInsights.R
@@ -84,6 +84,11 @@ getInsights <- function(object_id, token, metric, period='day', parms=NA, versio
         period <- period
   }
 
+  ## CHECK IF DATE IS MORE THAN TWO YEARS OLD
+  since_date <- as.Date(gsub('.*since=([0-9]{4}-[0-9]{2}-[0-9]{2}).*', parms, repl="\\1"))
+  if (Sys.Date() - since_date > 365 * 2){
+    message("Note: metrics older than 2 years may not be available through the API.")
+  }
 
   ### CREATE LIST OF REQUEST URLS
   url <- list()
